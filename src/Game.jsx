22ad8249d08f4fd5113dda4106bd347e800543d4
Parag Kharche <parag.kharche@oracle.com>
2024-09-11 22:40:25 +0530

    first commit

---
+++ src/Game.jsx
@@ -0,0 +1,14 @@
+import App from "./App";
+
+const Game = () => {
+  return (
+    <div className="game">
+      <App />
+      <div className="game-info">
+        <ol>{/*TODO*/}</ol>
+      </div>
+    </div>
+  );
+};
+
+export default Game;
---
+++ src/Square.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable react/prop-types */
+
+const Square = ({ value, onSquareClick }) => {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+};
+
+export default Square;
